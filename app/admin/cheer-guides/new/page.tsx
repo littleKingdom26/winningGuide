ac08f9c7723f9c5df3fa82aa94a018a89bb22c05
littlekingdom <wingindream@gmail.com>
2026-06-19 10:18:56 +0900

    빌드 수정

--- app/admin/cheer-guides/new/page.tsx
+++ app/admin/cheer-guides/new/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import CheerGuideFormContent from './CheerGuideFormContent';
 
 export default function CheerGuideFormPage() {
   return (
     <div className="p-6">
-      <CheerGuideFormContent />
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <CheerGuideFormContent />
+      </Suspense>
     </div>
   );
 }
